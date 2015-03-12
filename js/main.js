cb654f5383666d8f45433c055a450eb501c99bb5
Anna Trojanowska <a.s.trojanowska@gmail.com>
2015-03-11 21:12:34 -0400

    changed tear drop animation, added toggle nav

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var $hamBtn = $('.ham-btn');
+var $panel = $('.panel');
+var $tabs = $('.tabs');
+var $panels = $('.tab-panel');
+
+$hamBtn.on('click', function () {
+  $panel.toggleClass('js-panel-open');
+});
+
+$tabs.on('click', 'a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  $panels.filter(':not([hidden])').attr('hidden', true);
+  $(id).removeAttr('hidden');
+
+  $tabs.find('.js-current').removeClass('js-current');
+  $(this).addClass('js-current');
+});
