a9cf74dd3ee9a414e2bed06e16677f9a0a56a444
Anna Trojanowska <a.s.trojanowska@gmail.com>
2015-03-18 17:35:25 -0400

    created animations and transitions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ var $panel = $('.panel');
 var $tabs = $('.tabs');
 var $panels = $('.tab-panel');
 
+var $music = $('.music');
+var $leftnote = $('.leftnote');
+var $rightnote = $('.rightnote');
+var $doublenote = $('.doublenote');
+
+var $drawing = $('.drawing');
+var $pencil = $('.pencil');
+
+var $reading = $('.reading');
+var $bookopen = $('.bookopen');
+var $bookclosed = $('.bookclosed');
+
+
 $hamBtn.on('click', function () {
   $panel.toggleClass('js-panel-open');
 });
@@ -17,3 +30,19 @@ $tabs.on('click', 'a', function (e) {
   $tabs.find('.js-current').removeClass('js-current');
   $(this).addClass('js-current');
 });
+
+$drawing.on('click', function () {
+  $pencil.toggleClass('js-pencil-move');
+});
+
+$music.on('click', function () {
+    $leftnote.toggleClass('js-leftnote-appear');
+    $rightnote.toggleClass('js-rightnote-appear');
+    $doublenote.toggleClass('js-doublenote-appear');
+});
+
+$reading.on('click', function () {
+    $bookopen.toggleClass('js-bookopen-appear');
+    $bookclosed.toggleClass('js-bookclosed-appear');
+});
+
