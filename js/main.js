71e13495fea2e09eb1015be2cc9e993c9d321d45
Anna Trojanowska <a.s.trojanowska@gmail.com>
2015-04-01 13:29:41 -0400

    added back to top and view portfolio buttons

--- js/main.js
+++ js/main.js
@@ -46,3 +46,23 @@ $reading.on('click', function () {
     $bookclosed.toggleClass('js-bookclosed-appear');
 });
 
+var $tabs = $('.tabs');
+var $panels = $('.tab-panel');
+
+$tabs.on('click', 'a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  $panels.filter('.js-appear').removeClass('js-appear');
+  $(id).addClass('js-appear');
+
+  $tabs.find('.js-current').removeClass('js-current');
+  $(this).addClass('js-current');
+});
+
+$('.top-nav').localScroll();
+
+$('.top-scroll').localScroll();
+
+$('.top-scroll-button').localScroll();
+
